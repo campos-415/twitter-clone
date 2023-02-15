bb2d30224d10e5afa524f6a26c1b390e28e91e98
Cesar Campos <camposss.415@gmail.com>
2023-02-14 23:32:41 -0800

    creating the repository for the web app

---
+++ components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col 
+    items-center xl:items-start xl:w-[340px]
+    p-24 fixed h-full">
+      <div className="flex items-center 
+      justify-center w-14 h-14x hoverAnimation 
+      p-0 xl:ml-24 ">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} className=""/>
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active/>
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Message" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="List" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["rb.gy"],
+    unoptimized: true,
+  },
+};
