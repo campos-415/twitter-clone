4c78d5eb5591dbbfacb23bd3d2a4a430603a6404
Cesar Campos <camposss.415@gmail.com>
2023-02-26 13:55:40 -0800

    .

--- firebase.js
+++ firebase.js
@@ -26,13 +26,3 @@ const storage = getStorage();
 export default app;
 export { db, storage };
 
-// OLD FIREBASE SDK
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCngRNs12aS5un6Jje593LDSqk0VjV66xc",
-//   authDomain: "twitter-clone-80caf.firebaseapp.com",
-//   projectId: "twitter-clone-80caf",
-//   storageBucket: "twitter-clone-80caf.appspot.com",
-//   messagingSenderId: "1037949950112",
-//   appId: "1:1037949950112:web:3480528c2e4b7edbae11c5",
-//   measurementId: "G-8JRHP4VRXT"
-// };
