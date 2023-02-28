81d08d597707c995a53a5f8cc5db608348af64bc
Cesar Campos <camposss.415@gmail.com>
2023-02-28 03:40:47 -0800

    .

--- components/Widgets.jsx
+++ components/Widgets.jsx
@@ -9,44 +9,42 @@ import { useEffect, useState } from "react";
 import Trending from "./Trending";
 import { useRecoilState } from "recoil";
 import { userPosts } from "atoms/modalAtom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 
 function Widgets() {
   const { data: session } = useSession();
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useRecoilState(userPosts)
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useRecoilState(userPosts);
   const router = useRouter();
   // console.log(router)
 
   function getUserId(e, id, res) {
-    console.log(id)
-    setUser(res)
+    console.log(id);
+    setUser(res);
     if (id === session.user.uid) {
       router.push(`/user/${session.user.uid}`);
     } else {
       router.push(`/users/${id}`);
     }
-
   }
 
   useEffect(() => {
-    
     const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
-      setUsers(snapshot.docs.map((doc) => {
-        return {
-          id: doc.data().id,
-          tag: doc.data().tag,
-          userImg: doc.data().userImg,
-          username: doc.data().username,
-        }
-      }))
-    })
+      setUsers(
+        snapshot.docs.map((doc) => {
+          return {
+            id: doc.data().id,
+            tag: doc.data().tag,
+            userImg: doc.data().userImg,
+            username: doc.data().username,
+          };
+        })
+      );
+    });
 
-    return unsubscribe
-  },[])
-
-  
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="hidden lg:inline ml-8 xl:w-[450px] py-1 space-y-5">
@@ -64,19 +62,19 @@ function Widgets() {
       </div>
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Check my other cool Projects</h4>
-          <Trending />
-        <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
-          <Link href={"https://github.com/campos-415"} target={"_blank"}>
+        <Trending />
+        <Link href={"https://github.com/campos-415"} target={"_blank"}>
+          <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
             Github Account
-          </Link>
-          <FontAwesomeIcon icon={faGithub} className="w-9"/>
-        </div>
-        <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
-          <Link href={"https://github.com/campos-415"} target={"_blank"}>
+            <FontAwesomeIcon icon={faGithub} className="w-9" />
+          </div>
+        </Link>
+        <Link href={"https://github.com/campos-415"} target={"_blank"}>
+          <div className="hover:bg-white hover:bg-opacity-[0.03] px-4 py-3 cursor-pointer transition duration-200 ease-out flex items-center justify-between w-full text-[#1d9bf0] font-light">
             Linked In
-          </Link>
-          <FontAwesomeIcon icon={faLinkedinIn} className="w-9"/>
-        </div>
+            <FontAwesomeIcon icon={faLinkedinIn} className="w-9" />
+          </div>
+        </Link>
       </div>
       <div className="text-[#d9d9d9] space-y-3 bg-[#15181c] pt-2 rounded-xl w-11/12 xl:w-9/12">
         <h4 className="font-bold text-xl px-4">Who to Stalk</h4>
@@ -94,7 +92,9 @@ function Widgets() {
                 className="h-11 w-11 rounded-full mr-4"
                 alt="userImg"
               />
-              <div className="ml-4 leading-5 group" onClick={(e) => getUserId(e, res.id, res)} >
+              <div
+                className="ml-4 leading-5 group"
+                onClick={(e) => getUserId(e, res.id, res)}>
                 <h4 className="font-bold group-hover:underline">
                   {res?.username}
                 </h4>
